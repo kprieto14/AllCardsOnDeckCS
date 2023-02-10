aab0ff0ae8280991e3fd3136a80de644054c313e
Kristy Prieto <kprieto14@gmail.com>
2023-02-09 21:02:17 -0500

    Finished with explorer mode, off to to attempt adventure mode

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace AllCardsOnDeckCS
 {
@@ -6,7 +7,42 @@ namespace AllCardsOnDeckCS
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      //Creates new lists of the suits, ranks, and the deck list to combine the two to be added later through looping.
+      var suits = new List<string>() {"Clubs", "Diamonds", "Hearts", "Spades"};
+      var ranks = new List<string>() {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+      var deck = new List<string>(); 
+
+      //Creates a nested loop to assign all the ranks in each suit, and adds them to the deck list.
+      foreach (var suit in suits) 
+      {
+        foreach (var rank in ranks) 
+        {
+          var card = $"{rank} of {suit}";
+          deck.Add(card);
+        }
+      }
+      
+      var numberOfCards = deck.Count; 
+
+      //Establishes loop that will shuffle deck
+      for(var rightIndex = numberOfCards - 1; rightIndex >=1; rightIndex--) 
+      {
+        //Establishes the 2nd card to be randomly shuffled from the last card in deck
+        var randomNumberGenerator = new Random();
+        var leftIndex = randomNumberGenerator.Next(rightIndex);
+        //Matches the number index from loop and re-assigns the value with the corresponding value from the "deck" list
+        var leftCard = deck[leftIndex];
+        var rightCard = deck[rightIndex];
+        //Switches the left card with the right card to be shuffled until the loop reaches the last card
+        deck[rightIndex] = leftCard;
+        deck[leftIndex] = rightCard;
+      }
+
+      var firstCard = deck[0];
+      var secondCard = deck[1];
+
+      Console.WriteLine(firstCard);
+      Console.WriteLine(secondCard);
     }
   }
 }
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # AllCardsOnDeckCS
+
+My attempt at trying the PEDAC myself and see how much I could come up with on my own, unfortunately it seems I was still missing much information to help me out and I needed the video for much of the help :(
+
+P
+
+The deck should contain 52 cards, with 2 different lists of 4 suits and 13 ranks. They should be shuffled with the last card in the deck (rightIndex) being shuffled with a random card in the deck. 
+
+E
+
+Will need variables for Suits, Ranks, Indexes
+
+D
+
+Will need strings, loops, index, random.Next() function
+
+A
+
+Create a list for the 4 suits, create a list of the 13 ranks. Combine the two in a list statement of deck, create a loop then ADDS (deck.Add("string");) each rank with each suit. Then to implement the shuffling of cards, will need the count of cards (numberOfCards = some variable.Count) then do rightIndex = count - 1 to grab the last card in the "deck"/ list. leftindex will be 0 to grab the first card in the list. Here is the example the teacher gives:
+
+numberOfCards = length of our deck
+
+for rightIndex from numberOfCards - 1 down to 1 do:
+  leftIndex = random integer that is greater than or equal to 0 and LESS than rightIndex. See the section "How do we get a random integer")
+
+  Now swap the values at rightIndex and leftIndex by doing this:
+    leftCard = the value from deck[leftIndex]
+    rightCard = the value from deck[rightIndex]
+    deck[rightIndex] = leftCard
+    deck[leftIndex] = rightCard
+
+Will need the following to work with the shuffling:
+var randomNumberGenerator = new Random();
+var randomNumber = randomNumberGenerator.Next(52); with the 52 representing the 52 cards in the deck. Come back to Adventure mode later.
+
+C
